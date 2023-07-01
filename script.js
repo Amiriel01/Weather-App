c9b1d1c0373a84cf26614e8fe03bde4fc154dedb
Sarah Quinney <quinney.sarah@gmail.com>
2023-07-01 11:48:58 -0500

    format extended weather done

--- script.js
+++ script.js
@@ -3,23 +3,23 @@
 
 function populateExtendedForcast() {
     //append function to container to populate card data//
-    let extendedForcastContainer = document.querySelector(".extended-forcast-container");
+    extendedForcastContainer = document.querySelector(".extended-forcast-container");
     // console.log(extendedForcastContainer);
-    extendedForcastContainer.appendChild(createForcastCard("Image", "Friday", "Friday Forcast Info"));
+    extendedForcastContainer.appendChild(createForcastCard("Image", "Date", "Condition", "Temp Low", "Temp High","Temp Low", "Temp High"));
 
-    extendedForcastContainer.appendChild(createForcastCard("Image", "Saturday", "Saturday Forcast Info"));
+    extendedForcastContainer.appendChild(createForcastCard("Image", "Date", "Condition", "Temp Low", "Temp High","Temp Low", "Temp High"));
 
-    extendedForcastContainer.appendChild(createForcastCard("Image", "Sunday", "Sunday Forcast Info"));
+    extendedForcastContainer.appendChild(createForcastCard("Image", "Date", "Condition", "Temp Low", "Temp High","Temp Low", "Temp High"));
 
-    extendedForcastContainer.appendChild(createForcastCard("Image", "Monday", "Monday Forcast Info"));
+    extendedForcastContainer.appendChild(createForcastCard("Image", "Date", "Condition", "Temp Low", "Temp High","Temp Low", "Temp High"));
 
-    extendedForcastContainer.appendChild(createForcastCard("Image", "Tuesday", "Tuesday Forcast Info"));
+    extendedForcastContainer.appendChild(createForcastCard("Image", "Date", "Condition", "Temp Low", "Temp High","Temp Low", "Temp High"));
 }
 
 populateExtendedForcast();
 
 //create five day forcast cards function to create the cards//
-function createForcastCard(img, day, info) {
+function createForcastCard(img, day, info, lowF, highF, lowC, highC) {
     let extendedForcastContainer = document.querySelector(".extended-container");
 
     //create the day info container and attach it to extend container html//
@@ -27,37 +27,55 @@ function createForcastCard(img, day, info) {
     forcastFlex.classList.add("forcast-flex");
     extendedForcastContainer.appendChild(forcastFlex);
 
-    //create info-day container and append it to extendedForcastContainer//
-    let imgDayContainer = document.createElement("div");
-    imgDayContainer.classList.add("img-day");
-    forcastFlex.appendChild(imgDayContainer);
-
-    //create the img and day divs attach them to imgDayContainer//
-    let extendedWeatherImage = document.createElement("img");
-    extendedWeatherImage.classList.add("extended-weather-img");
-    extendedWeatherImage.setAttribute("src", img);
+    //create info-day container and append it to forcast flex//
+    // let imgDayContainer = document.createElement("div");
+    // imgDayContainer.classList.add("img-day");
+    // forcastFlex.appendChild(imgDayContainer);
 
     let extendedDay = document.createElement("div");
     extendedDay.classList.add("extended-day");
     extendedDay.innerText = day;
 
-    imgDayContainer.appendChild(extendedWeatherImage);
-    imgDayContainer.appendChild(extendedDay);
+    let extendedWeatherImage = document.createElement("img");
+    extendedWeatherImage.classList.add("extended-weather-img");
+    extendedWeatherImage.setAttribute("src", img);
 
-    //create the extendedWeatherForcast div and append it to the extendedForcastContainer//
-    let extendedWeatherForcast = document.createElement("div");
-    extendedWeatherForcast.classList.add("extended-weather-forcast");
-    extendedWeatherForcast.innerText = info;
-    forcastFlex.appendChild(extendedWeatherForcast);
+    forcastFlex.appendChild(extendedDay);
+    forcastFlex.appendChild(extendedWeatherImage);
+    
+
+    //create the extendedWeatherForcast div and append it to the forcast flex container//
+    let extendedContidion = document.createElement("div");
+    extendedContidion.classList.add("extended-weather-condition");
+    extendedContidion.innerText = info;
+    forcastFlex.appendChild(extendedContidion);
+
+    //create the temp low div and append it to the forcast flex container//
+    let tempLowF = document.createElement("div");
+    tempLowF.classList.add("temp-low-f");
+    tempLowF.innerText = lowF + '\u00B0 F';
+    forcastFlex.appendChild(tempLowF);
+
+    //create the temp high div and append it to the forcast flex container//
+    let tempHighF = document.createElement("div");
+    tempHighF.classList.add("temp-high-f");
+    tempHighF.innerText = highF + '\u00B0 F';
+    forcastFlex.appendChild(tempHighF);
+
+    //create the temp low div and append it to the forcast flex container//
+    let tempLowC = document.createElement("div");
+    tempLowC.classList.add("temp-low-c");
+    tempLowC.innerText = lowC + '\u00B0 C';
+    forcastFlex.appendChild(tempLowC);
+
+    //create the temp high div and append it to the forcast flex container//
+    let tempHighC = document.createElement("div");
+    tempHighC.classList.add("temp-high-c");
+    tempHighC.innerText = highC + '\u00B0 C';
+    forcastFlex.appendChild(tempHighC);
 
     return forcastFlex;
 }
-
-// async function logJSONData() {
-//     const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?q=Chicago&days=5&dt=2023-07-01&alerts=alerts%3Dyes&aqi=aqi%3Dno&key=8b8288369c154c74b09154532232806=${searchValue}`, { mode: 'cors' });
-//     const jsonData = await response.json();
-//     console.log(jsonData);
-//   }
 
 function locationSearch(e) {
     //set the button for the location name input to run location search//
@@ -88,6 +106,7 @@ function locationSearch(e) {
 
         //display icon on the page//
         .then(function (response) {
+            //creates current forcast data//
             let currentImage = document.querySelector("#current-image");
             currentImage.src = response.current.condition.icon;
 
@@ -102,20 +121,34 @@ function locationSearch(e) {
 
             let currentTemperatureC = document.querySelector("#current-temperature-c")
             currentTemperatureC.innerText = response.current.temp_c + '\u00B0 C'
+
+            //creates extended weather dates data//
+            let extendedDay1 = document.querySelector("#extended-day");
+            extendedDay1.innerText = response.forcast.forcastday[0].date;
+
+            let extendedDay2 = document.querySelector("#extended-day");
+            extendedDay2.innerText = response.forcast.forcastday[1].date;
+
+            //creates extended forcast icon data//
+            let extendedWeatherImage1 = document.querySelector("#extended-weather-img");
+            extendedWeatherImage1.src = response.forcast.forcastday[0].condition.icon;
+
+            let extendedWeatherImage2 = document.querySelector("#extended-weather-img");
+            extendedWeatherImage2.src = response.forcast.forcastday[1].condition.icon;
+
+            let extendedWeatherImage3 = document.querySelector("#extended-weather-img");
+            extendedWeatherImage3.src = response.forcast.forcastday[2].condition.icon;
+
+            let extendedWeatherImage4 = document.querySelector("#extended-weather-img");
+            extendedWeatherImage4.src = response.forcast.forcastday[3].condition.icon;
+
+            let extendedWeatherImage5 = document.querySelector("#extended-weather-img");
+            extendedWeatherImage5.src = response.forcast.forcastday[4].condition.icon;
+
+            
+
         })
 
-    // //display city name on the page//
-    // .then(function(response) {
-    //     let cityName = document.querySelector("#city-name")
-    //     cityName.innerText = response.location.name.region
-    // })
-
-    // .then(function (response) {
-    //     image.src = response.forcast.forcastday[0].original.url;
-    // })
-    // .catch (function (err) {
-    //     console.error(err);
-    // });
     return false;
 }
 locationSearch();
